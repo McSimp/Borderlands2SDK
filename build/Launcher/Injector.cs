66ccfa6e2676648b36694aaf9a9603fec44e0b0e
McSimp <simpsoholic@gmail.com>
2012-10-14 01:04:32 +1100

    Launcher now works!
    Except the SDK will attempt to load before the engine has loaded and has
    all sorts of problems. Will have to look into that at some point.

---
+++ build/Launcher/Injector.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Launcher
+{
+    public class RemoteMemory : IDisposable
+    {
+        private readonly IntPtr pMemory = IntPtr.Zero;
+        private readonly IntPtr hProcess;
+
+        public IntPtr Address
+        {
+            get
+            {
+                return this.pMemory;
+            }
+        }
+
+        public RemoteMemory(IntPtr hProcess, uint size)
+        {
+            this.hProcess = hProcess;
+            this.pMemory = Win32.VirtualAllocEx(
+                hProcess,
+                IntPtr.Zero,
+                size,
+                Win32.AllocationType.Commit | Win32.AllocationType.Reserve,
+                Win32.MemoryProtection.ReadWrite);
+
+            if (this.pMemory == IntPtr.Zero)
+            {
+                Win32.CloseHandle(hProcess);
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to allocate memory in remote process");
+            }
+        }
+
+        public void WriteMemory(byte[] pBuffer)
+        {
+            UIntPtr lpNumberOfBytesWritten;
+
+            bool result = Win32.WriteProcessMemory(
+                this.hProcess,
+                this.pMemory,
+                pBuffer,
+                (uint)pBuffer.Length,
+                out lpNumberOfBytesWritten);
+
+            if (!result || lpNumberOfBytesWritten != (UIntPtr)(pBuffer.Length))
+            {
+                Win32.CloseHandle(hProcess);
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to write memory in remote process");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.pMemory == IntPtr.Zero)
+            {
+                return;
+            }
+
+            Win32.VirtualFreeEx(this.hProcess, this.pMemory, 0, Win32.FreeType.Release);
+        }
+    }
+
+    public static class Injector
+    {
+        public static void Inject(string libPath, Process proc)
+        {
+            // Check if DLL is present
+            if (!File.Exists(libPath))
+            {
+                throw new FileNotFoundException("Injectee DLL not found", libPath);
+            }
+
+            // Get a handle on LoadLibraryW so we can call it in the game process later
+            IntPtr hKernel32 = Win32.GetModuleHandle("kernel32.dll");
+            IntPtr hLoadLib = Win32.GetProcAddress(hKernel32, "LoadLibraryW");
+            if (hKernel32 == IntPtr.Zero || hLoadLib == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to get handle to LoadLibraryW");
+            }
+
+            // Get a native handle to the process
+            Win32.ProcessAccessFlags access = 
+                  Win32.ProcessAccessFlags.CreateThread
+                | Win32.ProcessAccessFlags.VMOperation
+                | Win32.ProcessAccessFlags.VMRead
+                | Win32.ProcessAccessFlags.VMWrite
+                | Win32.ProcessAccessFlags.QueryInformation;
+
+            IntPtr hProcess = Win32.OpenProcess(access, false, proc.Id);
+            if (hProcess == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to get handle to game process");
+            }
+
+            // Create unicode version of the path to the DLL
+            byte[] unicodePath = Encoding.Unicode.GetBytes(libPath);
+
+            // Allocate memory in the remote process for the path
+            using (RemoteMemory remoteMem = new RemoteMemory(hProcess, (uint)unicodePath.Length))
+            {
+                // Write the path to the memory
+                remoteMem.WriteMemory(unicodePath);
+
+                // Execute LoadLibraryW on the path in the remote process
+                IntPtr hThread = Win32.CreateRemoteThread(
+                    hProcess,
+                    IntPtr.Zero,
+                    0U,
+                    hLoadLib,
+                    remoteMem.Address,
+                    0U,
+                    IntPtr.Zero);
+                if (hThread == IntPtr.Zero)
+                {
+                    Win32.CloseHandle(hProcess);
+                    throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to execute remote LoadLibraryW");
+                }
+
+                // Wait for the thread to do its thing, error out if it doesn't manage to
+                if (Win32.WaitForSingleObject(hThread, (uint)Win32.ThreadWaitValue.Infinite) != (uint)Win32.ThreadWaitValue.Object0)
+                {
+                    Win32.CloseHandle(hThread);
+                    Win32.CloseHandle(hProcess);
+                    throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to wait for remote thread");
+                }
+
+                // Get the result of LoadLibraryW - throw exception if it didn't appear to load
+                IntPtr hInjectedDLL;
+                if (!Win32.GetExitCodeThread(hThread, out hInjectedDLL) || hInjectedDLL == IntPtr.Zero)
+                {
+                    Win32.CloseHandle(hThread);
+                    Win32.CloseHandle(hProcess);
+                }
+            }
+
+            // Looks like we made it!
+        }
+    }
+}
--- build/Launcher/frmLauncher.cs
+++ build/Launcher/frmLauncher.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace Launcher
 {
@@ -17,50 +20,90 @@ namespace Launcher
         {
             if (txtGamePath.TextLength > 0)
             {
-                return Path.GetDirectoryName(txtGamePath.Text);
+                string currPath = Path.GetDirectoryName(txtGamePath.Text);
+                if (Directory.Exists(currPath))
+                {
+                    return currPath;
+                }
             }
 
-            RegistryKey steamAppKey = Registry.LocalMachine.OpenSubKey(@"\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 49520");
+            RegistryKey steamAppKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 49520");
             if (steamAppKey != null)
             {
                 string installDir = (string)steamAppKey.GetValue("InstallLocation");
                 if (!string.IsNullOrEmpty(installDir))
                 {
-                    return installDir;
-                }
-            }
-
-            RegistryKey steamKey = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam");
-            if (steamKey != null)
-            {
-                string steamPath = (string)steamKey.GetValue("SteamPath");
-                if (!string.IsNullOrEmpty(steamPath))
-                {
-                    string bl2Path = Path.GetFullPath(Path.Combine(steamPath, "steamapps", "common", "Borderlands2"));
+                    string bl2Path = Path.GetFullPath(Path.Combine(installDir, "Binaries", "Win32"));
                     if (Directory.Exists(bl2Path))
                     {
                         return bl2Path;
                     }
                 }
             }
+
+            RegistryKey steamKey = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
+            if (steamKey != null)
+            {
+                string steamPath = (string)steamKey.GetValue("SteamPath");
+                if (!string.IsNullOrEmpty(steamPath))
+                {
+                    string bl2Path = Path.GetFullPath(Path.Combine(steamPath, "steamapps", "common", "Borderlands2", "Binaries", "Win32"));
+                    if (Directory.Exists(bl2Path))
+                    {
+                        return bl2Path;
+                    }
+                }
+            }
+
             return string.Empty;
+        }
+
+        private string GetAbsoluteGamePath()
+        {
+            string parent = GetStartPath();
+            if (!string.IsNullOrEmpty(parent))
+            {
+                return Path.Combine(parent, "Borderlands2.exe");
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        private void SetupRegistry()
+        {
+            // Check if we already have our key setup
+            RegistryKey SDKSubKey = Registry.CurrentUser.OpenSubKey(@"Software\BL2SDK", RegistryKeyPermissionCheck.ReadWriteSubTree);
+            if (SDKSubKey == null)
+            {
+                // Key doesn't exist, create it
+                SDKSubKey = Registry.CurrentUser.CreateSubKey(@"Software\BL2SDK", RegistryKeyPermissionCheck.ReadWriteSubTree);
+            }
+
+            string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            SDKSubKey.SetValue("BinPath", Path.Combine(currentDir + "\\"));
+            SDKSubKey.SetValue("LuaPath", Path.Combine(currentDir, "lua\\"));
+            SDKSubKey.SetValue("GwenPath", Path.Combine(currentDir, "gwen\\"));
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = this.GetStartPath();
+            openFileDialog.InitialDirectory = GetStartPath();
             openFileDialog.Filter = "Borderlands 2 (*.exe)|*.exe";
             DialogResult result = openFileDialog.ShowDialog();
             if (result == DialogResult.OK)
             {
                 txtGamePath.Text = openFileDialog.FileName;
-                txtGamePath.ScrollToCaret();
             }
         }
 
-        private void btnLaunch_Click(object sender, EventArgs e)
+        private void btnLaunch_Click(object sender, EventArgs ea)
         {
+            SetupRegistry();
+            
             string gamePath = txtGamePath.Text;
             if (!File.Exists(gamePath))
             {
@@ -76,22 +119,87 @@ namespace Launcher
 
             string gameDir = Path.GetDirectoryName(gamePath);
 
+            Win32.STARTUPINFO lpStartupInfo = new Win32.STARTUPINFO();
+
             Environment.SetEnvironmentVariable("SteamGameId", "49520");
             Environment.SetEnvironmentVariable("SteamAppId", "49520");
 
-            WinAPI.STARTUPINFO lpStartupInfo = new WinAPI.STARTUPINFO();
-            WinAPI.PROCESS_INFORMATION lpProcessInformation = new WinAPI.PROCESS_INFORMATION();
+            Win32.PROCESS_INFORMATION lpProcessInformation;
 
-            if (WinAPI.CreateProcess(null, string.Format("{0} -nolauncher", gamePath), IntPtr.Zero, IntPtr.Zero, false, (uint)WinAPI.CreateProcessFlags.CREATE_SUSPENDED, IntPtr.Zero, gameDir, ref lpStartupInfo, out lpProcessInformation))
+            bool result = Win32.CreateProcess(
+                gamePath,
+                "-nolauncher",
+                IntPtr.Zero,
+                IntPtr.Zero,
+                false,
+                Win32.CreateProcessFlags.CreateSuspended,
+                IntPtr.Zero,
+                gameDir,
+                ref lpStartupInfo, 
+                out lpProcessInformation);
+
+            if (result)
             {
-                btnBrowse.Enabled = false;
-                btnBrowse.Text = "Waiting for Borderlands 2 to launch...";
+                btnLaunch.Enabled = false;
+                btnLaunch.Text = "Injecting into Borderlands 2...";
 
-                WinAPI.CloseHandle(lpProcessInformation.hProcess);
-                WinAPI.CloseHandle(lpProcessInformation.hThread);
+                string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process bl2Proc = Process.GetProcessById(lpProcessInformation.dwProcessId);
 
-                // Todo: Injection code lawl
+                // Yuck.
+                try
+                {
+                    Injector.Inject(Path.Combine(currentDir, "BL2SDKDLL.dll"), bl2Proc);
+                }
+                catch (Win32Exception e)
+                {
+                    MessageBox.Show("Failed to inject SDK into Borderlands 2. Exception Message = " + e.Message + " Code = " + e.NativeErrorCode, "Failed to launch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    Win32.TerminateProcess(lpProcessInformation.hProcess, 0);
+                    
+                    btnLaunch.Enabled = true;
+                    btnLaunch.Text = "Launch Game";
+                    return;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Failed to inject SDK into Borderlands 2. Exception = " + e.Message, "Failed to launch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    Win32.TerminateProcess(lpProcessInformation.hProcess, 0);
+
+                    btnLaunch.Enabled = true;
+                    btnLaunch.Text = "Launch Game";
+                    return;
+                }
+                finally
+                {
+                    Win32.CloseHandle(lpProcessInformation.hProcess);
+                    Win32.CloseHandle(lpProcessInformation.hThread);
+                }
+
+                foreach (ProcessThread thread in bl2Proc.Threads)
+                {
+                    IntPtr hThread = Win32.OpenThread(
+                        Win32.ThreadAccessFlags.SuspendResume,
+                        false,
+                        (uint)thread.Id);
+
+                    Win32.ResumeThread(hThread);
+                    Win32.CloseHandle(hThread);
+                }
+
+                btnLaunch.Enabled = true;
+                btnLaunch.Text = "Injected";
             }
+            else
+            {
+                MessageBox.Show("Failed to launch Borderlands 2. Error Code = " + Marshal.GetLastWin32Error(), "Failed to Launch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmLauncher_Load(object sender, EventArgs e)
+        {
+            txtGamePath.Text = GetAbsoluteGamePath();
         }
     }
 }
